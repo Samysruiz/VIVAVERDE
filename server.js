9d4bf83cd867913245b3c6239db8e853ba98d007
Samysruiz <samysruiz@gmail.com>
2026-05-23 10:33:42 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express  = require('express');
 const fetch    = require('node-fetch');
-const path     = require('path');
+const path     = require('path'); 
 const { loadLibrary, getIndex, buildContext } = require('./drive-loader');
 const app  = express();
 const PORT = process.env.PORT || 3000;
